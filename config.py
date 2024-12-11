02a00c371abf5511fbb9ac3da1986377f83cc8ad
GetBabass <164517438+GetBabass@users.noreply.github.com>
2024-12-11 16:28:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 import os
 
 #Bot token @Botfather
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7500647170:AAGciQLc3xSk9UvR8aE8oV_unv_oa7uga5A")
 
 #Your API ID from my.telegram.org
-API_ID = int(os.environ.get("API_ID", ""))
+API_ID = int(os.environ.get("API_ID", "26801787"))
 
 #Your API Hash from my.telegram.org
-API_HASH = os.environ.get("API_HASH", "")
+API_HASH = os.environ.get("API_HASH", "85d9a2a75a01faf11a745dfd2f4a8d55")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://marvinavenue736:21oAeV6ItXuDjxPd@cluster0.eh3kv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
